a979ae5797aed46b7282766c36d59beecea93bee
Artur Marcinkowski <arturmarcinkowski@interia.pl>
2018-03-30 14:12:15 +0200

    Add files and code for exercise 9.5

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+var buttons = document.getElementsByClassName("button");
+
+for (var i = 0; i < buttons.length; i++) {
+	 console.log('Napis w przycisku to: ' + buttons[i].innerHTML);
+}
